fb25ea9785266ad658b4642c38eef1c61dbd04fa
Fatih-dev74 <fatihcigdem74100@gmail.com>
2024-05-23 22:28:12 +0200

    -

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -29,9 +29,11 @@ form.addEventListener("submit", function (event) {
     headers: {
       "Content-Type": "application/json",
     },
+    
     // Les données de l'utilisateur sont placées dans le JSON.
     body: JSON.stringify(user),
   })
+
     // nous obtenons la réponse
     .then((response) => {
       // Dans le cas où le statut est de 200, les données sont conservées dans le JSON.
@@ -47,6 +49,7 @@ form.addEventListener("submit", function (event) {
         error.innerText = "Utilisateur inconnu";
       }
     })
+
     // Il faut récupérer le token dans les données du JSON, le stocker, puis le rediriger.
     .then((data) => {
       sessionStorage.setItem("token", data.token);
